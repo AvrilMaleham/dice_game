2147d44af61017f8c85ea12419ba6099b2c91def
Avril Maleham <avrilmaleham@outlook.com>
2024-05-13 16:32:06 +1200

    feat: basic frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,35 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Home() {
+  const [num1, setNum1] = useState<number>();
+  const [num2, setNum2] = useState<number>();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-       dice game
-      </div>
+    <main className="flex min-h-screen flex-col items-center">
+      <h1 className="p-10 text-2xl font-bold">Dice Game</h1>
+
+      <p>How many sides per dice?</p>
+      <input
+        onChange={(e) => {
+          setNum1(parseInt(e.target.value));
+        }}
+        type="number"
+        className="m-5 text-black"
+      ></input>
+      <p>How many dice?</p>
+      <input
+        onChange={(e) => {
+          setNum2(parseInt(e.target.value));
+        }}
+        type="number"
+        className="m-5 text-black"
+      ></input>
+      <button className="m-5 p-2 bg-white text-black">Go!</button>
+
+      <p className="m-5">Num1: {num1}</p>
+      <p className="m-5">Num2: {num2}</p>
     </main>
   );
 }
